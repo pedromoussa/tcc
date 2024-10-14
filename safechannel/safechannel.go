8f51aaefc2a565a42d044bbedae05bbca14408ba
pedromoussa <pedropaulo.moussa@gmail.com>
2024-10-14 10:43:23 -0300

    adiciona canal de notificacoes

--- safechannel/safechannel.go
+++ safechannel/safechannel.go
@@ -8,12 +8,14 @@ import (
 type SafeChannel[T any] struct {
 	sendCh chan T
 	recvCh chan T
+	notifyCh  chan Notification[T]
 	mu     sync.Mutex
 	closed bool
 }
 
 func NewSafeChannel[T any](bufferSize int) *SafeChannel[T] {
 	var sendCh, recvCh chan T
+	notifyCh := make(chan Notification[T], 10)
 
 	if bufferSize > 0 {
 		sendCh = make(chan T, bufferSize)
@@ -26,6 +28,7 @@ func NewSafeChannel[T any](bufferSize int) *SafeChannel[T] {
 	sc := &SafeChannel[T]{
 		sendCh: sendCh,
 		recvCh: recvCh,
+		notifyCh: notifyCh,
 		closed: false,
 	}
 
@@ -42,7 +45,6 @@ func (sc *SafeChannel[T]) forwardMessages() {
 			return
 		}
 
-		// Forward message to recvCh
 		sc.recvCh <- value
 	}
 }
@@ -52,14 +54,19 @@ func (sc *SafeChannel[T]) Send(value T) error {
 	defer sc.mu.Unlock()
 
 	if sc.closed {
+		sc.notify(Notification[T]{ReturnValue: -1, Message: "send on closed channel", Value: value})
 		return errors.New("send on closed channel")
 	}
 
 	select {
 	case sc.sendCh <- value:
+		sc.notify(Notification[T]{ReturnValue: 0, Message: "sent successfully", Value: value})
 		return nil
 	default:
-		return errors.New("channel buffer full")
+		sc.notify(Notification[T]{ReturnValue: -1, Message: "channel buffer full", Value: value})
+		sc.sendCh <- value
+		sc.notify(Notification[T]{ReturnValue: 0, Message: "sent after waiting", Value: value})
+		return nil
 	}
 }
 
@@ -67,6 +74,7 @@ func (sc *SafeChannel[T]) Receive() (T, error) {
 	value, ok := <-sc.recvCh
 	if !ok {
 		var zero T
+		sc.notify(Notification[T]{ReturnValue: 0, Message: "receive on closed channel"})
 		return zero, errors.New("receive on closed channel")
 	}
 	return value, nil
@@ -76,6 +84,7 @@ func (sc *SafeChannel[T]) Close() error {
 	sc.mu.Lock()
 	if sc.closed {
 		sc.mu.Unlock()
+		sc.notify(Notification[T]{ReturnValue: -1, Message: "channel already closed"})
 		return errors.New("channel already closed")
 	}
 
@@ -104,6 +113,7 @@ func CaseReceive[T any](sc *SafeChannel[T], onReceive func(T)) SelectCaseFunc {
 		select {
 		case msg, ok := <-sc.recvCh:
 			if !ok {
+				sc.notify(Notification[T]{ReturnValue: -1, Message: "receive on closed channel", Value: msg})
 				return true, -1, nil, errors.New("receive on closed channel")
 			}
 			if onReceive != nil {
@@ -119,13 +129,30 @@ func CaseReceive[T any](sc *SafeChannel[T], onReceive func(T)) SelectCaseFunc {
 func CaseSend[T any](sc *SafeChannel[T], msg T) SelectCaseFunc {
 	return func() (bool, int, interface{}, error) {
 		if sc.closed {
+			sc.notify(Notification[T]{ReturnValue: -1, Message: "send on closed channel", Value: msg})
 			return true, -1, nil, errors.New("send on closed channel")
 		}
 		select {
 		case sc.sendCh <- msg:
 			return true, 0, nil, nil
 		default:
+			sc.notify(Notification[T]{ReturnValue: -1, Message: "channel buffer full", Value: msg})
 			return true, -1, nil, errors.New("channel buffer full")
 		}
+	}
+}
+
+type Notification[T any] struct {
+	ReturnValue int
+	Message     string
+	CallerID    string
+	Value       T
+}
+
+func (sc *SafeChannel[T]) notify(notification Notification[T]) {
+	select {
+	case sc.notifyCh <- notification:
+	default:
+		// Ignore the notification if notifyCh is full
 	}
 }
