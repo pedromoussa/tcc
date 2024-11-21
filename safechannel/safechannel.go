7dcea9f10373d487bf797cdd51c3e022d8d87070
pedromoussa <pedropaulo.moussa@gmail.com>
2024-11-21 10:48:14 -0300

    add monitoramento buffer do canal; add funcao dentro do case default; add notificacao forwarding de msgs;

--- safechannel/safechannel.go
+++ safechannel/safechannel.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 type SafeChannel[T any] struct {
@@ -12,9 +13,10 @@ type SafeChannel[T any] struct {
 	notifyCh  chan Notification[T]
 	mu     sync.Mutex
 	closed bool
+	messagesInBuffer int64
 }
 
-func MakeSafeChannel[T any](bufferSize ...int) *SafeChannel[T] {
+func MakeSafechannel[T any](bufferSize ...int) *SafeChannel[T] {
 	var sendCh, recvCh chan T
 	var size = getBufferSize(bufferSize)
 	
@@ -31,6 +33,10 @@ func MakeSafeChannel[T any](bufferSize ...int) *SafeChannel[T] {
 	return sc
 }
 
+func (sc *SafeChannel[T]) GetMessageCount() int64 {
+	return atomic.LoadInt64(&sc.messagesInBuffer)
+}
+
 func (sc *SafeChannel[T]) forwardMessages() {
 	for {
 		value, ok := <-sc.sendCh
@@ -43,7 +49,17 @@ func (sc *SafeChannel[T]) forwardMessages() {
 			return
 		}
 
-		sc.recvCh <- value
+		// sc.recvCh <- value
+
+		// Attempt to forward the message to recvCh (same logic as in Send())
+		select {
+		case sc.recvCh <- value:
+			sc.notify(Notification[T]{ReturnValue: 0, Message: "received successfully", Value: value})
+		default:
+			sc.notify(Notification[T]{ReturnValue: -1, Message: "channel buffer full", Value: value})
+			sc.recvCh <- value
+			sc.notify(Notification[T]{ReturnValue: 0, Message: "received after waiting", Value: value})
+		}
 	}
 }
 
@@ -59,10 +75,12 @@ func (sc *SafeChannel[T]) Send(value T) error {
 	select {
 	case sc.sendCh <- value:
 		sc.notify(Notification[T]{ReturnValue: 0, Message: "sent successfully", Value: value})
+		atomic.AddInt64(&sc.messagesInBuffer, 1)
 		return nil
 	default:
 		sc.notify(Notification[T]{ReturnValue: -1, Message: "channel buffer full", Value: value})
 		sc.sendCh <- value
+		atomic.AddInt64(&sc.messagesInBuffer, 1)
 		sc.notify(Notification[T]{ReturnValue: 0, Message: "sent after waiting", Value: value})
 		return nil
 	}
@@ -75,6 +93,7 @@ func (sc *SafeChannel[T]) Receive() (T, error) {
 		sc.notify(Notification[T]{ReturnValue: 0, Message: "receive on closed channel"})
 		return zero, errors.New("receive on closed channel")
 	}
+	atomic.AddInt64(&sc.messagesInBuffer, -1)
 	return value, nil
 }
 
@@ -95,16 +114,37 @@ func (sc *SafeChannel[T]) Close() error {
 
 type SelectCaseFunc func() (bool, int, interface{}, error)
 
+// Trying to ensure default case is evaluated only when no other case is ready
 func Select(cases ...SelectCaseFunc) (int, interface{}, error) {
-	for {
-		for _, c := range cases {
+	var defaultIdx int = -1
+	for i, c := range cases {
 			ok, idx, value, err := c()
 			if ok {
-				return idx, value, err
+					return idx, value, err
 			}
-		}
+			if idx == -1 {
+					defaultIdx = i
+			}
 	}
+
+	if defaultIdx != -1 {
+			_, idx, value, err := cases[defaultIdx]()
+			return idx, value, err
+	}
+
+	return -1, nil, nil
 }
+
+// func Select(cases ...SelectCaseFunc) (int, interface{}, error) {
+// 	for {
+// 		for _, c := range cases {
+// 			ok, idx, value, err := c()
+// 			if ok {
+// 				return idx, value, err
+// 			}
+// 		}
+// 	}
+// }
 
 func CaseReceive[T any](sc *SafeChannel[T], onReceive func(T)) SelectCaseFunc {
 	return func() (bool, int, interface{}, error) {
@@ -140,8 +180,12 @@ func CaseSend[T any](sc *SafeChannel[T], msg T) SelectCaseFunc {
 	}
 }
 
-func CaseDefault() SelectCaseFunc {
+// Default case will now perform an action
+func CaseDefault(action func()) SelectCaseFunc {
 	return func() (bool, int, interface{}, error) {
+			if action != nil {
+					action()
+			}
 			return true, -1, nil, nil
 	}
 }
